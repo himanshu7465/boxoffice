b4d064e98ff420dc99e28d76348dd8b19dbf611c
himanshu7465 <108960811+himanshu7465@users.noreply.github.com>
2022-07-31 12:50:21 +0530

    added route and switch

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
 /* eslint-disable */
 
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Navs from './components/Navs';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
 
-const App = () => {
-  return <div>App</div>;
-};
+function App() {
+  return (
+    <div>
+      <Navs />
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+
+        <Route>
+          <div>Not found</div>
+        </Route>
+      </Switch>
+    </div>
+  );
+}
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,18 @@
 /* eslint-disable */
 
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
